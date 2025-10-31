c15a62faa2151c364fcdcc75808f388c38901101
Mansa Obi <khornermarkets@Mansas-MBP.lan>
2025-10-31 10:18:38 -0500

    Update tweet templates with Uniswap links and consistent formatting

--- src/utils/tweetTemplates.js
+++ src/utils/tweetTemplates.js
@@ -2,24 +2,30 @@
  * Tweet Templates for Pixel Ponies
  */
 
-export const REGISTRATION_TWEET_TEMPLATE = `I just registered to race on #PixelPonies and got 1B $PONY. Join @pxponies tg to get yours.
+export const REGISTRATION_TWEET_TEMPLATE = `I just registered to race on #PixelPonies and got 1B $PONY. Join @pxponies telegram and register to get yours NOW!
 
 TG: https://t.me/pixelponies
+
 Website: pxponies.com
 
-Get $PONY: Token CA: https://basescan.org/address/0x6ab297799335E7b0f60d9e05439Df156cf694Ba7
+Get $PONY: https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x6ab297799335E7b0f60d9e05439Df156cf694Ba7&chain=base
+
+Token CA: https://basescan.org/address/0x6ab297799335E7b0f60d9e05439Df156cf694Ba7
+
 Game CA: https://basescan.org/address/0x2B4652Bd6149E407E3F57190E25cdBa1FC9d37d8
 
 Blockchain: $BASE`;
 
 export const RACE_TWEET_TEMPLATE = (horseNumber, amount) => `I just bet ${amount} $PONY that horse #${horseNumber} would win on @PixelPonies
 
-Blockchain: $BASE
-
 TG: https://t.me/pixelponies
+
 Website: pxponies.com
 
-Get $PONY: Token CA: https://basescan.org/address/0x6ab297799335E7b0f60d9e05439Df156cf694Ba7
+Get $PONY: https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x6ab297799335E7b0f60d9e05439Df156cf694Ba7&chain=base
+
+Token CA: https://basescan.org/address/0x6ab297799335E7b0f60d9e05439Df156cf694Ba7
+
 Game CA: https://basescan.org/address/0x2B4652Bd6149E407E3F57190E25cdBa1FC9d37d8
 
 Blockchain: $BASE`;
